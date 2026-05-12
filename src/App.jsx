b473ce24cdba99fb3a0fdb7e8e587729e85ffe56
Jakaria030 <gulamjakaria99@gmail.com>
2026-05-13 01:12:10 +0600

    feat: folder structure implement

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,41 @@
+import { Route, Routes } from "react-router";
+
+// Layouts
+import MainLayout from "./layouts/MainLayout";
+import AdminLayout from "./layouts/AdminLayout";
+
+// Main Pages
+import Home from "./pages/main/Home/Home";
+import Subjects from "./pages/main/Subjects/Subjects";
+import Leaderboard from "./pages/main/Leaderboard/Leaderboard";
+import Contact from "./pages/main/Contact/Contact";
+import Login from "./pages/main/Login/Login";
+
+// Admin Pages
+import Dashboard from "./pages/admin/Dashboard/Dashboard";
+
+
+
 const App = () => {
     return (
-        <div>
-            Mutex Mind
-        </div>
+        <Routes>
+            {/* ================= MAIN WEBSITE ================= */}
+            <Route path="/" element={<MainLayout />}>
+                <Route index element={<Home />} />
+                <Route path="/subjects" element={<Subjects />} />
+                <Route path="/leaderboard" element={<Leaderboard />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/login" element={<Login />} />
+            </Route>
+
+            {/* ================= ADMIN PANEL ================= */}
+            <Route
+                path="/admin"
+                element={<AdminLayout />}
+            >
+                <Route index element={<Dashboard />} />
+            </Route>
+        </Routes>
     );
 };
 
